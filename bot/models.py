4e52319d7e586134c1a4b58dfe7347f6c5332cd1
Max Brauer <max.brauer@picturepipe.com>
2021-10-08 22:33:26 +0200

    Started database model

---
+++ bot/models.py
@@ -0,0 +1,23 @@
+from peewee import SqliteDatabase, Model, CharField,  SmallIntegerField
+
+db = SqliteDatabase('people.db')
+
+STATE_NEW = 0
+STATE_ACCEPTED = 1
+STATE_DECLINED = 2
+
+class Suggestion(Model):
+    summary = CharField()
+    discord_id = CharField()
+    state =  SmallIntegerField(default=STATE_NEW)
+
+    class Meta:
+        database = db
+
+    def accept(self):
+        self.state = STATE_ACCEPTED
+        self.save()
+
+    def decline(self):
+        self.state = STATE_DECLINED
+        self.save()
