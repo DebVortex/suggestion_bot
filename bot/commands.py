c778cd71522eace2acc73ae03881b11aff41a777
Max Brauer <max.brauer@picturepipe.com>
2021-10-16 22:55:49 +0200

    Added brief and help for /help

--- bot/commands.py
+++ bot/commands.py
@@ -1,29 +1,37 @@
 from discord.ext import commands
 
 
-@commands.command()
+@commands.command(
+    brief="Show new suggestions",
+    help="Shows a list of all suggestions which has neither been accepted nor declined."
+)
 async def show_new(ctx, *args):
     ...
 
 
-@commands.command()
+@commands.command(brief="Show accepted suggestions")
 async def show_accepted(ctx, *args):
     ...
 
 
-@commands.command()
+@commands.command(brief="Show declined suggestions")
 async def show_declined(ctx, *args):
     ...
 
 
-@commands.command()
+@commands.command(brief="Accept a suggestion")
 async def accept(ctx, id):
     ...
 
 
-@commands.command()
+@commands.command(brief="Decline a suggestion")
 async def decline(ctx, id):
     ...
 
 
-COMMANDS = [show_new, show_accepted, show_declined, accept, decline]
+@commands.command(brief="Set state of a suggestion back to new")
+async def renew(ctx, id):
+    ...
+
+
+COMMANDS = [show_new, show_accepted, show_declined, accept, decline, renew]
